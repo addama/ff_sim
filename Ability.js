177d7cabf4e7ca1f9d5e728f3ff18ea8c7a79421
Addama <addama.sanders@gmail.com>
2014-10-08 13:54:37 -0700

    Enhanced the displayStats() function to provide detailed info in the Ability and its Effect payload

--- Ability.js
+++ Ability.js
@@ -6,30 +6,55 @@ function Ability(type) {
 	this.baseDamage = (config.abilities[type].baseDamage) ? config.abilities[type].baseDamage : config.variables.defaults.baseDamage;
 	this.effect = (config.abilities[type].effect.type) ? config.abilities[type].effect.type : config.variables.defaults.effect;
 	this.target = (config.abilities[type].effect.target) ? config.abilities[type].effect.target : config.variables.defaults.target;
+		
+	this.payLoad = this.makeEffect();
 	
-	//console.log(this.displayName());
+	//console.log(this.payLoad);
 };
 
 Ability.prototype = {
 	constructor: Ability,
 	
-	validateType: function(type) {
-		if (this.lists.types.indexOf(type)) {
-			return true;
-		}
-		return false;
-	},
-	
 	displayName: function(verbose) {
 		function titleCase(str) {
 			return str.replace(/\b\w+/g,function(s){return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();});
 		}
+		
 		var result = titleCase(this.title) + ' (' + this.element + ' ' + this.effect + ')';
 		return result;
 	},
 	
+	displayStats: function() {
+		if (this.payLoad.type === 'dot') {
+			result = 'Causes ' + this.payLoad.getGranularAmount() + ' ' + this.element + ' damage to ' + this.target + ' every turn for ' + this.payLoad.duration + ' turns'; 
+		}
+		
+		if (this.payLoad.type === 'hot') {
+			result = 'Heals ' + this.target + ' for ' + this.payLoad.getGranularAmount() + ' damage every turn for ' + this.payLoad.duration + ' turns'; 
+		}
+		
+		if (this.payLoad.type === 'buff') {
+			result = 'Applies a ' + this.payLoad.buffTarget + ' buff to ' + this.target + ' for ' + this.payLoad.duration + ' turns'; 
+		}
+		
+		if (this.payLoad.type === 'debuff') {
+			result = 'Applies a ' + this.payLoad.buffTarget + ' debuff to ' + this.target + ' for ' + this.payLoad.duration + ' turns'; 
+		}
+		
+		if (this.payLoad.type === 'heal') {
+			result = 'Heals ' + this.target + ' for ' + this.payLoad.getGranularAmount() + ' damage'; 
+		}
+		
+		if (this.payLoad.type === 'damage') {
+			result = 'Deals ' + this.payLoad.getGranularAmount() + ' ' + this.payLoad.element + ' damage to ' + this.target; 
+		}
+		return result;
+		
+	},
+	
 	makeEffect: function() {
-		var effect = new Effect(this.effect, this.element, this.effectTarget, this.baseDamage);
+		console.log(this.baseDamage);
+		var effect = new Effect(this.effect, this.element, this.target, this.baseDamage);
 		return effect;
 	},
 }
