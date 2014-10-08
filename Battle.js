2947ec2237c753ec8af3707791273a3cc57d38ad
Addama <addama.sanders@gmail.com>
2014-10-08 10:30:28 -0700

    Battle.js now completely relies on Configuration.js for its data. Also, made many of the set up functions more flexible.

--- Battle.js
+++ Battle.js
@@ -1,62 +1,43 @@
 function Battle(partySize) {
-	// http://en.wikipedia.org/wiki/Box-drawing_character
-	if (partySize && partySize > 0) {
-		this.partySize = partySize;
-	}
-	for (var i = 0; i < this.partySize; i++) {
-		var combatantLeft = this.createCombatant('left', i.toString());
-		var combatantRight = this.createCombatant('right', i.toString());
-		this.roster.left.push(combatantLeft);
-		this.roster.right.push(combatantRight);
-	}
-	this.turnOrder = [];
-	this.makeTurnOrder();
-	console.log(this.turnOrder);
-	this.currentCombatant = 0;
-	console.log(this.whoseTurnIsIt());
+	// Wait a second for config.json to be loaded into config
+	var app = this;
+	setTimeout(function() {
+		// Set the party size
+		app.partySize = config.variables.partySize;
+		if (partySize && partySize > 1) app.partySize = partySize;
+		
+		// Define the teams
+		for (var i = 0; i < config.variables.teams; i++) {
+			app.roster[config.variables.defaults.teamNames[i]] = [];
+		}
+		
+		// Build the teams
+		for (var i = 0; i < app.partySize; i++) {
+			for (var team in app.roster) {
+				var combatant = app.createCombatant(team, i.toString());
+				app.roster[team].push(combatant);
+			}
+			
+		}
+		app.makeTurnOrder();
+		console.log(app.turnOrder);
+		app.currentCombatant = 0;
+		console.log(app.whoseTurnIsIt());
+	}, 100);
 };
 
 Battle.prototype = {
 	constructor: Battle,
-	partySize: 6,
 	
-	roster: {
-		left: [],
-		right: [],
-	},
-	
-	lists: {
-		type: [ 'tank', 'healer', 'meleeDex', 'meleeStr', 'rangedInt', 'rangedDex' ],
-		element: [ 'life', 'death', 'holy', 'evil', 'fire', 'ice', 'earth', 'air', 'physical', 'psychic' ],
-		gender: [ 'male', 'female' ],
-		race: [ 'human', 'dwarf', 'elf', 'orc', 'troll', 'halfling', 'giant' ],
-	},
-	
-	getRandomType: function() {
-		var rand = Math.floor(Math.random() * this.lists.type.length);
-		return this.lists.type[rand];
-	},
-	
-	getRandomElement: function() {
-		var rand = Math.floor(Math.random() * this.lists.element.length);
-		return this.lists.element[rand];
-	},
-	
-	getRandomGender: function() {
-		var rand = Math.floor(Math.random() * this.lists.gender.length);
-		return this.lists.gender[rand];
-	},	
-	
-	getRandomRace: function() {
-		var rand = Math.floor(Math.random() * this.lists.race.length);
-		return this.lists.race[rand];
-	},
+	roster: {},
+	turnOrder: [],
+	currentCombatant: 0,
 	
 	createCombatant: function(team, slot, type, element, race, gender) {
-		if (!type) type = this.getRandomType();
-		if (!element) element = this.getRandomElement();
-		if (!race) race = this.getRandomRace();
-		if (!gender) gender = this.getRandomGender();
+		if (!type) type = config.getRandomType();
+		if (!element) element = config.getRandomElement();
+		if (!race) race = config.getRandomRace();
+		if (!gender) gender = config.getRandomGender();
 		if (!team) return false;
 		if (!slot) return false;
 		
@@ -71,16 +52,14 @@ Battle.prototype = {
 				return ((x < y) ? -1 : ((x > y) ? 1 : 0));
 			});
 		}	
-	
-		for (var person in this.roster.left) {
-			this.turnOrder.push({slot: person, team: 'left', speed: this.roster.left[person].zipperStat('speed')});
-		}
 		
-		for (var person in this.roster.right) {
-			this.turnOrder.push({slot: person, team: 'right', speed: this.roster.right[person].zipperStat('speed')});
+		for (var team in this.roster) {
+			for (var person in this.roster[team]) {
+				this.turnOrder.push({slot: person, 'team': team, speed: this.roster[team][person].zipperStat('speed')});
+			}
+			this.turnOrder = sortBySpeed(this.turnOrder);
+			this.turnOrder.reverse();
 		}
-		this.turnOrder = sortBySpeed(this.turnOrder);
-		this.turnOrder.reverse();
 	},
 	
 	whoseTurnIsIt: function() {
